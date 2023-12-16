d254c0269e302068ff764ea50a21f49b0b76f592
Tim Gibbon <tng@chegwin.org>
2023-12-16 16:52:57 +0000

    Update permissions

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -7,8 +7,10 @@ data "aws_iam_policy_document" "s3_write_access" {
     sid    = "AllowS3RWScript"
     effect = "Allow"
     actions = [
-      "s3:Get*",
-      "s3:Put*"
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:ListObjectsV2",
+      "s3:CopyObject"
     ]
     resources = [aws_s3_bucket.www.arn]
   }
@@ -25,26 +27,6 @@ resource "aws_iam_user_policy_attachment" "s3_write_access" {
   policy_arn = aws_iam_policy.s3_write_access.arn
 }
 
-
-resource "aws_iam_access_key" "s3_write_access" {
-  user = aws_iam_user.s3_write_access.name
-}
-
-resource "aws_secretsmanager_secret" "s3_write_access" {
-  name        = "aws-credentials-s3-write-${var.domain_name}"
-  description = "AWS Credentials to write to ${var.domain_name} bucket"
-}
-
-
-resource "aws_secretsmanager_secret_version" "s3_write_access" {
-  secret_id     = aws_secretsmanager_secret.s3_write_access.id
-  secret_string = <<EOF
-    {
-    "AWS_ACCESS_KEY_ID": "${aws_iam_access_key.s3_write_access.id}",
-    "AWS_SECRET_ACCESS_KEY": "${aws_iam_access_key.s3_write_access.secret}"
-    }
-EOF
-}
 
 module "github-oidc" {
   source  = "terraform-module/github-oidc-provider/aws"
